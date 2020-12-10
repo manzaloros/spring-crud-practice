da1ab302a951f620375aa8f0e87a6d8996e96e2f
manzaloros <zacharymansellcello@gmail.com>
2020-12-10 13:17:53 -0600

    Add spring initialzr files

---
+++ src/main/java/com/galvanize/springcrudpractice/SpringCrudPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.springcrudpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudPracticeApplication.class, args);
+	}
+
+}
